6aeee54be72cce16032d429ae35684a34dc6d1a2
KEDAR DAVE <kedardave2902@gmail.com>
2023-06-05 12:20:54 +0530

    Saved Easy.cpp

--- Problems/easy.cpp
+++ Problems/easy.cpp
@@ -570,3 +570,12 @@ int majorityElement(vector<int> &nums)
 
     return candidate;
 }
+int titleToNumber(string columnTitle)
+{
+    int result = 0;
+    for (char c : columnTitle)
+    {
+        result = result * 26 + (c - 'A' + 1);
+    }
+    return result;
+}
